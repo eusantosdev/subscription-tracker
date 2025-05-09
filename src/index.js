f041003002416972521ed32dc9629c18955bb260
eusantosdev <eusantosdev@gmail.com>
2025-05-09 11:35:50 -0300

    feat: rotas criadas!

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import express from 'express';
 
 import { PORT } from './config/env.js'
 
+import authRouter from './routers/auth.routes.js';
+import subscriptionRouter from './routers/subscription.routes.js';
+import userRouter from './routers/user.routes.js';
+
 const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/subscription', subscriptionRouter);
+app.use('/api/v1/users', userRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
---
+++ src/routers/auth.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+const authRouter = Router();
+
+authRouter.get('/', (req, res) => res.send({ title : 'GET all auth' }));
+
+authRouter.get('/:id', (req, res) => res.send({ title: 'GET auth by id'}));
+
+authRouter.post('/:id', (req, res) => res.send({ title: 'CREATE auth'}));
+
+authRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE auth by id'}));
+
+authRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE auth by id'}));
+
+export default authRouter;
