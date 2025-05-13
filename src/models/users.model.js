77139ddacf7b16973fe59fd249694b74c2699265
eusantosdev <eusantosdev@gmail.com>
2025-05-13 10:16:48 -0300

    refactor: Armazenamento automatico

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -22,7 +22,7 @@ const userSchema = new Schema({
         required: [true, 'Password is required'],
         minlength: 6,
     }
-});
+}, { timestamps: true });
 
 const user = mongoose.model('User', userSchema);
 
